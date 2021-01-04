53e2112496654636537782dd8fab28835e770e51
skytrophy` <sky.business.wtg@gmail.com>
2021-01-04 18:13:14 +0900

    table

---
+++ public/js/scripts.js
@@ -0,0 +1,11 @@
+constapp=newVue({
+    el:'#app',
+    data:{
+    items:[
+    {no:'1',name:'そら',sex:'♂',age:'8',kind:'キジトラ',favorite:'犬の人形'},
+    {no:'2',name:'りく',sex:'♂',age:'7',kind:'長毛種',favorite:'人間'},
+    {no:'3',name:'うみ',sex:'♀',age:'6',kind:'ミケ',favorite:'高級ウェットフード'},
+    {no:'4',name:'こうめ',sex:'♀',age:'4',kind:'サビ',favorite:'横取りフード'},
+    ]
+    }
+    })
